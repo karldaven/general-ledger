e123b77f80fccbb4f63f3ffe8f18179993bb4cd3
karldaven <karldaven@gmail.com>
2022-10-14 01:36:14 +0900

    Add MySQL docker config.

---
+++ docker-mysql/db/init.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS `ledger`;
+DROP TABLE IF EXISTS `transaction`;
+
+CREATE TABLE `ledger` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `currency` varchar(3) NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `transaction` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `ledger_id` int unsigned NOT NULL,
+  `transaction_datetime` datetime NOT NULL,
+  `is_credit` tinyint unsigned NOT NULL,
+  `amount` decimal(10,2) NOT NULL,
+  `balance` decimal(10,2) unsigned NOT NULL,
+  `description` varchar(45) DEFAULT NULL,
+  `transaction_date_time` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
